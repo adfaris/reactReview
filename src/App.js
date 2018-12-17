0afb3d8ebd45e8565c04aba9403f2f7d2a6d766a
A.D.Faris <adfaris@gmail.com>
2018-12-17 12:45:40 -0500

    deleted anchor

--- src/App.js
+++ src/App.js
@@ -17,14 +17,6 @@ class App extends Component {
               Welcome {helloworld}
             </h1>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     )
