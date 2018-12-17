ba1d80f1a823e00d84d8beceb52da8973cb86e48
A.D.Faris <adfaris@gmail.com>
2018-12-17 10:53:25 -0500

    react_review

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
+    // const name = ['AD Faris', 'John', 'Dina']
+    const helloworld = 'Welcome to the road to react'
+
+    // helloworld.text = 'bye bye'
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1>
+              Welcome {helloworld}
+            </h1>
           </p>
           <a
             className="App-link"
@@ -21,8 +27,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
