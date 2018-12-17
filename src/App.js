6252881b9f28664ee608411c2613e52513ee8e70
A.D.Faris <adfaris@gmail.com>
2018-12-17 13:58:32 -0500

    map and render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
-
+const name = ['Dina', 'Love', 'Snukum']
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: '4',
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 class App extends Component {
   render() {
     // const name = ['AD Faris', 'John', 'Dina']
-    const helloworld = 'Welcome to the road to react'
+    // const helloworld = 'Welcome to the road to react'
 
     // helloworld.text = 'bye bye'
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <h1>
-              Welcome {helloworld}
-            </h1>
-          </p>
-        </header>
+        {/* // render name */}
+        {name}
+        {/* // map through array and print title */}
+        {list.map(function(item) {
+          return (
+            <div>
+              <span>
+                <a href={item.url}>
+                  {' '}{item.title}
+                </a>
+              </span>
+              <span>
+                {item.author}
+              </span>
+              <span>
+                {item.num_comments}
+              </span>
+              <span>
+                {' '}{item.objectID}
+              </span>
+            </div>
+          )
+        })}
       </div>
     )
   }
