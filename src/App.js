66223b1a54fac35547922449b0e683ec023d5531
A.D.Faris <adfaris@gmail.com>
2018-12-28 11:51:20 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ let defaultOptions = {
 }
 const DEFAULT_QUERY = 'redux'
 
-const PATH_BASE = 'https://hn.algolia.com/api'
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
 // const url = `${PATH_BASE} ${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
