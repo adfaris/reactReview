1ec7a7484ff1f218db8f8187bd47e182c20c5aed
A.D.Faris <adfaris@gmail.com>
2018-12-29 16:37:25 -0500

    refactoring split constants

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ import './App.css'
 //   },
 //   body: null,
 // }
-const DEFAULT_QUERY = 'redux'
-const DEFAULT_HPP = '100'
+// const DEFAULT_QUERY = 'redux'
+// const DEFAULT_HPP = '100'
 
-const PATH_BASE = 'https://hn.algolia.com/api/v1'
-// const PATH_BASE = 'https://hn.foobar.com/api/v1'
-const PATH_SEARCH = '/search'
-const PARAM_SEARCH = 'query='
-const PARAM_PAGE = 'page='
-const PARAM_HPP = 'hitsPerPage='
+// const PATH_BASE = 'https://hn.algolia.com/api/v1'
+// // const PATH_BASE = 'https://hn.foobar.com/api/v1'
+// const PATH_SEARCH = '/search'
+// const PARAM_SEARCH = 'query='
+// const PARAM_PAGE = 'page='
+// const PARAM_HPP = 'hitsPerPage='
+
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP,
+} from './constants/index.js'
 // const url = (`${PATH_BASE} ${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE})`
 
 // function isSearched(searchTerm) {
